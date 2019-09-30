0861f7e160a2360775d065f39ada2c884799359e
Mustafa Hathiyari <mustafa.hathiyari@ff.com>
2019-09-30 14:15:18 -0700

    broken code

--- pure_pursuit.cpp
+++ pure_pursuit.cpp
@@ -7,6 +7,7 @@
 float Lfc = 2,k =0.1,L = 2.9, kv = 1;
 using namespace std;
 namespace plt = matplotlibcpp;
+
 struct state
 {
     float x;
@@ -32,13 +33,17 @@ struct state
     }
     void update(float a,float delta,float dt){
      x =  x +  v * cos(w) * dt;
-     y =  y +  v * sin(  w) * dt;
+     y =  y +  v * sin(w) * dt;
      w =   w +  v / L * tan(delta) * dt;
+     //w = mod2pi(w);
      v =  v + a * dt;
      x_rear =  x - ((L / 2) * cos(  w));
      y_rear =  y - ((L / 2) * sin(  w));
     }
 };
+inline double mod2pi(double theta){
+    return theta - 2*M_PI*floor(theta/2/M_PI); 
+}
 
 float pure_pursuit(int curr_indx,int pindx,state veh,vector<float> cx,vector<float> cy){
     int tx=0,ty=0;
@@ -48,11 +53,15 @@ float pure_pursuit(int curr_indx,int pindx,state veh,vector<float> cx,vector<flo
         tx = cx[curr_indx];
         ty = cy[curr_indx];
     }else{
-        return 0;
+        tx = -1;
+        ty = -1;
+        //indx = cx.size()-1;
     }
-    int alpha = atan2f(ty-veh.y_rear,tx-veh.x_rear);
+    float alpha = atan2f(ty-veh.y_rear,tx-veh.x_rear)-veh.w;
+    alpha = mod2pi(alpha);
     float look_ahead_dist = k*veh.v + Lfc;
-    float delta = atan2f(2*L*sin(alpha)/look_ahead_dist,1);
+    float delta = atan2f(2*L*sin(alpha)/look_ahead_dist,1.0);
+    delta = mod2pi(delta);
     return delta;
 
 }
@@ -62,7 +71,7 @@ int calc_lookahead_pt(state veh,vector<float> cx,vector<float> cy,int pindx){
     if(pindx ==0){
         float min_distance = FLT_MAX;
         for(int i = 0;i<cx.size();i++){
-            float distance = veh.distance(cx[i],cy[i]);
+            float distance = veh.distance_rear(cx[i],cy[i]);
             if(min_distance > distance){
                 min_distance = distance;
                 indx = i;
@@ -72,28 +81,30 @@ int calc_lookahead_pt(state veh,vector<float> cx,vector<float> cy,int pindx){
     }else
     {
         indx = old_nearest_point;
-        float distance_now = veh.distance(cx[indx],cy[indx]);
+        float distance_now = veh.distance_rear(cx[indx],cy[indx]);
         while(1){
-            indx+1<cx.size() ? indx++ : indx;
-            float distance_new = veh.distance(cx[indx],cy[indx]);
+            indx = indx+1<cx.size() ? indx++ : indx;
+            float distance_new = veh.distance_rear(cx[indx],cy[indx]);
             if(distance_now<distance_new) break;
             distance_now = distance_new;
         }
         old_nearest_point = indx;
     }
-    float temp =0;//gain on vel
+    float temp =0;
     float look_ahead_dist = k*veh.v + Lfc;
-    while (look_ahead_dist > temp){
+    while (look_ahead_dist > temp && indx+1 < cx.size()){
         temp = veh.distance_rear(cx[indx],cy[indx]);
         indx ++;
     }
     return indx;  
 }
 
-int pid_vel(float target,float vel){
+float pid_vel(float target,float vel){
     return kv*(target-vel);
 }
 int main(){
+    cout<< "while";
+
     // generate trajectory
     vector<float> cx((50)/0.1,0.0);
     for(float i = 1; i< cx.size();i++){
@@ -115,8 +126,11 @@ int main(){
     vector<float> x,y,w,v,t;
     while (time < max_time && curr_indx<cx.size()){
         pindx = curr_indx;
+        cout<< "while";
         curr_indx = calc_lookahead_pt(veh,cx,cy,pindx);
+        cout<< "index";        
         delta = pure_pursuit(curr_indx,pindx,veh,cx,cy);
+        cout<< "purs";        
         a = pid_vel(target_speed, veh.v);
         veh.update(a,delta,dt);
         cout<<veh.x-cx[curr_indx] << " " << veh.y-cy[curr_indx] << " " << time <<endl;
